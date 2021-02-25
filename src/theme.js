c9b683a57b602c1d9c5ca30fa9cbe167319cec93
Yevgeniya Alexeyeva <y.alekseyeva@gmail.com>
2021-02-25 17:39:40 +0600

    hw-11-part1-added

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+import colors from './colors';
+
+const ref = {
+  body: document.querySelector('body'),
+  buttonStart: document.querySelector('button[data-action="start"]'),
+  buttonStop: document.querySelector('button[data-action="stop"]'),
+};
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function changeColorTheme() {
+  const timerId = setInterval(() => {
+      ref.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+
+  ref.buttonStart.removeEventListener('click', changeColorTheme);
+  
+  ref.buttonStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    ref.buttonStart.addEventListener('click', changeColorTheme);
+  });
+}
+
+ref.buttonStart.addEventListener('click', changeColorTheme);
